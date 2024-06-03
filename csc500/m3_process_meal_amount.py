d09a2dcc3a2c6cfe8cedb88019c3e35f0e9afa1e
rajanivudumula <niharika.koppolu@gmail.com>
2024-06-02 21:54:09 -0400

    Added meal_price <= 0 condition

--- csc500/m3_process_meal_amount.py
+++ csc500/m3_process_meal_amount.py
@@ -3,10 +3,13 @@ import logging as log
 # Dictionaries are most appropriate data structure for the purpose
 tax_summary = {'meal_price': float(input("Enter the meal price : ")), 'waiter_incentive': 0.18, 'sales_tax': 0.07}
 
-tip_amount = tax_summary['waiter_incentive'] * tax_summary['meal_price']
-tax_amount = tax_summary['sales_tax'] * tax_summary['meal_price']
+if tax_summary['meal_price'] <= 0:
+    raise "Meal price can't be less than or equal to 0"
+else:
+    tip_amount = tax_summary['waiter_incentive'] * tax_summary['meal_price']
+    tax_amount = tax_summary['sales_tax'] * tax_summary['meal_price']
 
-print("Meal price   : {0:0.2f}".format(tax_summary['meal_price']))
-print("Tip amount   : {0:0.2f}".format(tip_amount))
-print("Tax amount   : {0:0.2f}".format(tax_amount))
-print("Total Amount : {0:0.2f}".format(tax_summary['meal_price'] + tip_amount + tax_amount))
+    print("Meal price   : {0:0.2f}".format(tax_summary['meal_price']))
+    print("Tip amount   : {0:0.2f}".format(tip_amount))
+    print("Tax amount   : {0:0.2f}".format(tax_amount))
+    print("Total Amount : {0:0.2f}".format(tax_summary['meal_price'] + tip_amount + tax_amount))
